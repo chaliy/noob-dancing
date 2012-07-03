8dd06ca51a4f8f98232ab62eb7cae2637f168f3e
Mike Chaliy <mike@chaliy.name>
2012-07-03 16:15:18 -0400

    Some improvements to detect_cycle

--- Lohika/detect_cycle.py
+++ Lohika/detect_cycle.py
@@ -4,23 +4,15 @@ class Node:
 		self.data = data
 		self.next = next
 
-def print_list(head):
-	while head:
-		print head.data,		
-		head = head.next
-	print
-
 def detect_cycle(head):
     a = head
     b = head.next
-    while a and b:
-        print a.data + " - " + b.data
-        if a == b:
-            return True
+    while a and b and a != b:    
+        print a.data + " - " + b.data           
         a = a.next
         b = b.next.next if b.next else None        
         
-    return False
+    return a == b
     
 n6 = Node("A6", None)
 n5 = Node("A5", n6)
@@ -33,4 +25,15 @@ n5.next = n2
 
 # n1 -> n2 -> n3 -> n4 -> n5 -> n2 -> n3
 
-print detect_cycle(nh)
+print "with cycle result: " + str(detect_cycle(nh))
+
+n23 = Node("A3", None)
+n22 = Node("A2", n23)
+n2h = Node("A1", n22)
+
+print "without cycle result: " + str(detect_cycle(n2h))
+
+n3h = Node("A1", None)
+n3h.next = n3h
+
+print "with cycle 1el result: " + str(detect_cycle(n3h))
